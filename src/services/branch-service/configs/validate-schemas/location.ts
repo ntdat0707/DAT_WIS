0c8b459ded015dbac435d24b57aca5d912751a39
tronglv <lvtrong1993@gmail.com>
2020-07-17 22:41:29 +0700

    feat(#78): create location

---
+++ src/services/branch-service/configs/validate-schemas/location.ts
@@ -0,0 +1,28 @@
+import Joi from '@hapi/joi';
+
+import { ELocationStatus } from '../../../../utils/consts';
+
+const createLocationSchema = Joi.object({
+  name: Joi.string()
+    .required()
+    .label('name'),
+  phone: Joi.string()
+    .required()
+    .label('phone'),
+  email: Joi.string()
+    .email()
+    .required()
+    .label('email'),
+  status: Joi.string()
+    .valid(ELocationStatus.ACTIVE, ELocationStatus.INACTIVE)
+    .required()
+    .label('status'),
+  city: Joi.string().label('city'),
+  district: Joi.string().label('district'),
+  ward: Joi.string().label('ward'),
+  address: Joi.string().label('address'),
+  latitude: Joi.number().label('latitude'),
+  longitude: Joi.number().label('longitude')
+});
+
+export { createLocationSchema };
