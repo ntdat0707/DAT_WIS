e5578b163a1fd7627a857f4a821df9b093d89e75
tronglv <lvtrong1993@gmail.com>
2020-07-18 17:11:29 +0700

    feat(Auth Midleware): add location ids and companyId

--- src/repositories/postresql/models/index.ts
+++ src/repositories/postresql/models/index.ts
@@ -3,11 +3,15 @@ import { StaffModel } from './staff-model';
 import { CompanyModel } from './company-model';
 import { CustomerModel } from './customer-model';
 import { LocationModel } from './location';
+import { StaffLocationModel } from './staff-location-model';
 
 StaffModel.hasOne(CompanyModel, { foreignKey: 'ownerId', as: 'hasCompany' });
 CompanyModel.belongsTo(StaffModel, { foreignKey: 'ownerId', as: 'owner' });
 
 CompanyModel.hasMany(LocationModel, { foreignKey: 'companyId', sourceKey: 'id', as: 'locations' });
 LocationModel.belongsTo(CompanyModel, { foreignKey: 'companyId', as: 'company' });
+
+StaffModel.belongsToMany(LocationModel, { through: StaffLocationModel, as: 'workingLocations', foreignKey: 'staffId' });
+LocationModel.belongsToMany(StaffModel, { through: StaffLocationModel, as: 'staffs', foreignKey: 'locationId' });
 
 export { sequelize, StaffModel, CompanyModel, CustomerModel, LocationModel };
