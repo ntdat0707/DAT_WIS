eb892b3bb666499e5a87bc9143f7b53fa7b43429
tronglv <lvtrong1993@gmail.com>
2020-07-23 01:13:51 +0700

    feat(#108): configs appointment enum status and status metric

--- src/utils/consts/index.ts
+++ src/utils/consts/index.ts
@@ -23,4 +23,16 @@ enum ELocationStatus {
   INACTIVE = 'inactive'
 }
 
-export { buildingEnvs, ELocales, EEnvironments, EGender, ELocationStatus };
+enum EAppointmentStatus {
+  NEW = 'new',
+  CONFIRMED = 'confirmed',
+  CANCELED = 'canceled'
+}
+const AppointmentStatusMetric = {
+  // NEW: { CONFIRMED: { up: true, down: true }, CANCELED: { up: true, down: false } }
+  new: { confirmed: true, canceled: true },
+  confirmed: { new: true, canceled: true },
+  canceled: { new: false, canceled: false }
+};
+
+export { buildingEnvs, ELocales, EEnvironments, EGender, ELocationStatus, EAppointmentStatus, AppointmentStatusMetric };
