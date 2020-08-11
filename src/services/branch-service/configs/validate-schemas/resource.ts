8bc2cbb80e3db957bbb1d535b69e77d66840ee61
Tu Nguyen Quoc <tunguyenq@gmail.com>
2020-08-11 23:08:52 +0700

    fixed: validate

--- src/services/branch-service/configs/validate-schemas/resource.ts
+++ src/services/branch-service/configs/validate-schemas/resource.ts
@@ -3,8 +3,8 @@ import Joi from 'joi';
 const createResourceSchema = Joi.object({
   locationId: Joi.string().required().label('locationId'),
   name: Joi.string().required().label('name'),
-  excerpt: Joi.string().required().label('excerpt'),
-  description: Joi.string().required().label('description'),
+  excerpt: Joi.string().allow(null).label('excerpt'),
+  description: Joi.string().allow(null).label('description'),
   serviceIds: Joi.array().items(Joi.string()).required()
 });
 
