f8776265b5411fff22b89a9499395d442e3fa027
sonkt12 <Son Nguyen>
2020-08-18 14:17:35 +0700

    fix: fix validate create customer

--- src/services/customer-service/configs/validate-schemas/customer.ts
+++ src/services/customer-service/configs/validate-schemas/customer.ts
@@ -3,9 +3,9 @@ import { EGender } from '../../../../utils/consts';
 
 const createCustomerSchema = Joi.object({
   fullName: Joi.string().required().label('fullName'),
-  gender: Joi.number().integer().required().valid(EGender.FEMALE, EGender.MALE, EGender.UNISEX).label('gender'),
+  gender: Joi.number().integer().allow(null).valid(EGender.FEMALE, EGender.MALE, EGender.UNISEX).label('gender'),
   phone: Joi.string().regex(/^\d+$/).required().label('phone'),
-  email: Joi.string().email().label('email'),
+  email: Joi.string().allow(null).email().label('email'),
   birthDate: Joi.string().isoDate().label('birthDate'),
   passportNumber: Joi.string().label('passportNumber'),
   address: Joi.string().label('address')
