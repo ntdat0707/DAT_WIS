bf8dde086ee051625bc1ccb79680944159f5ead6
sonkt12 <Son Nguyen>
2020-09-04 10:48:24 +0700

    feat(#219): add upload avatar staff

--- src/services/staff-service/routes/staff-route.ts
+++ src/services/staff-service/routes/staff-route.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 
 import { isAuthenticated } from '../../../utils/middlewares/staff/auth';
 import { StaffController } from '../controllers/staff-controller';
+import { uploadAsMiddleware } from '../../../utils/file-manager';
 require('dotenv').config();
 
 export class StaffRoutes {
@@ -14,10 +15,15 @@ export class StaffRoutes {
   private config(): void {
     this.router.get('/get-staff/:staffId?', this.staffController.getStaff);
     this.router.get('/get-staffs', isAuthenticated, this.staffController.getStaffs);
-    this.router.post('/create', isAuthenticated, this.staffController.createStaff);
+    this.router.post('/create', isAuthenticated, uploadAsMiddleware('avatar'), this.staffController.createStaff);
     this.router.get('/get-all-staffs', isAuthenticated, this.staffController.getAllStaffs);
     this.router.delete('/delete-staff/:staffId?', isAuthenticated, this.staffController.deleteStaff);
     this.router.post('/create-staffs', isAuthenticated, this.staffController.createStaffs);
-    this.router.put('/update/:staffId', isAuthenticated, this.staffController.updateStaff);
+    this.router.put(
+      '/update/:staffId',
+      isAuthenticated,
+      uploadAsMiddleware('avatar'),
+      this.staffController.updateStaff
+    );
   }
 }
