0d34c2c12a86b1acf5efc2ce1effc49aef348be7
dat <dat02.nguyen@unibiz.io>
2020-10-27 09:08:10 +0700

    fix list invoices log

--- src/services/sale-service/routes/invoice-route.ts
+++ src/services/sale-service/routes/invoice-route.ts
@@ -13,10 +13,6 @@ export class SaleRoutes {
     this.router.get('/get-all-receipt', isAuthenticated, this.invoiceController.getAllReceipt);
     this.router.get('/get-receipt/:receiptId', isAuthenticated, this.invoiceController.getReceipt);
     this.router.post('/create-invoice-log', isAuthenticated, this.invoiceController.createInvoiceLog);
-    this.router.get(
-      '/get-list-invoice-log/:customerWisereId',
-      isAuthenticated,
-      this.invoiceController.getListInvoicesLog
-    );
+    this.router.get('/get-list-invoice-log/:staffId', isAuthenticated, this.invoiceController.getListInvoicesLog);
   }
 }
