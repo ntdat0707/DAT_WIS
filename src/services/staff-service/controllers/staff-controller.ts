14fba87a4480b470cc13793306af51fa1305b89b
Huynh Xuan Oanh <oanh.huynh@unibiz.io>
2020-11-19 16:07:57 +0700

    fix unaccent

--- src/services/staff-service/controllers/staff-controller.ts
+++ src/services/staff-service/controllers/staff-controller.ts
@@ -36,7 +36,7 @@ import {
   PositionModel,
   RoleModel
 } from '../../../repositories/postgres/models';
-
+import { Unaccent } from '../../../utils/unaccent';
 import {
   staffIdSchema,
   createStaffSchema,
@@ -252,10 +252,11 @@ export class StaffController {
         ];
       }
       if (req.query.searchValue) {
+        const unaccentSearchValue = Unaccent(req.query.searchValue);
         query.where = {
           [Op.or]: [
             Sequelize.literal(
-              `unaccent(concat("StaffModel"."first_name", ' ', "StaffModel"."last_name")) ilike unaccent('%${req.query.searchValue}%')`
+              `unaccent(concat("StaffModel"."last_name", ' ', "StaffModel"."first_name")) ilike '%${unaccentSearchValue}%'`
             ),
             Sequelize.literal(`"StaffModel"."staff_code" ilike '%${req.query.searchValue}%'`),
             Sequelize.literal(`"StaffModel"."phone" like '%${req.query.searchValue}%'`),
