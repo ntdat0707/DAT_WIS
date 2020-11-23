0d6adde2aea70b7c16b291d6033ec0a6c669844f
dat <dat02.nguyen@unibiz.io>
2020-11-23 08:35:36 +0000

    cap nhat treatment api

--- src/services/treatment-service/routes/diagnosis-routes.ts
+++ src/services/treatment-service/routes/diagnosis-routes.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import { isAuthenticated } from '../../../utils/middlewares/staff/auth';
-import { DiagnosticController } from '../controllers/diagnosis-conroller';
+import { DiagnosticController } from '../controllers/diagnosis-controller';
 
 require('dotenv').config();
 
@@ -16,7 +16,7 @@ export class DiagnosticRoutes {
     this.router.post('/create-diagnostic', this.diagnosticsController.createDiagnostic);
     this.router.post('/create-diagnosis', this.diagnosticsController.createDiagnosis);
     this.router.get('/get-teeth/:teethId', isAuthenticated, this.diagnosticsController.getTeeth);
-    this.router.get('/get-diagnosis', isAuthenticated, this.diagnosticsController.getDiagnosis);
+    this.router.get('/get-diagnosis', isAuthenticated, this.diagnosticsController.getAllDiagnosis);
     this.router.put('/update-diagnosis/:diagnosisId', isAuthenticated, this.diagnosticsController.updateDiagnosis);
     this.router.post('/diagnosis/add-color/:diagnosticId', this.diagnosticsController.addColor);
   }
