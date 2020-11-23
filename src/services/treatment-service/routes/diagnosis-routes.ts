b501732977568fa9e46df3f970e60a63951ab6ae
dat <dat02.nguyen@unibiz.io>
2020-11-23 08:54:18 +0000

    cap nhat treatment api

--- src/services/treatment-service/routes/diagnosis-routes.ts
+++ src/services/treatment-service/routes/diagnosis-routes.ts
@@ -16,7 +16,7 @@ export class DiagnosticRoutes {
     this.router.post('/create-diagnostic', this.diagnosticsController.createDiagnostic);
     this.router.post('/create-diagnosis', this.diagnosticsController.createDiagnosis);
     this.router.get('/get-teeth/:teethId', isAuthenticated, this.diagnosticsController.getTeeth);
-    this.router.get('/get-diagnosis', isAuthenticated, this.diagnosticsController.getAllDiagnosis);
+    this.router.get('/get-all-diagnosis', isAuthenticated, this.diagnosticsController.getAllDiagnosis);
     this.router.put('/update-diagnosis/:diagnosisId', isAuthenticated, this.diagnosticsController.updateDiagnosis);
     this.router.post('/diagnosis/add-color/:diagnosticId', this.diagnosticsController.addColor);
   }
