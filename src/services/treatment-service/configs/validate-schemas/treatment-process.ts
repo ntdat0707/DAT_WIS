94763966566faf8f0e4ebce15fde30b3de597d9e
kimanh <anh.tran@unibiz.io>
2020-12-01 14:44:34 +0700

    thêm labo vào api update treatment process

--- src/services/treatment-service/configs/validate-schemas/treatment-process.ts
+++ src/services/treatment-service/configs/validate-schemas/treatment-process.ts
@@ -2,7 +2,7 @@ import Joi from 'joi';
 import { EStatusProcedure } from '../../../../utils/consts';
 
 const createTreatmentProcessSchema = Joi.object({
-  name: Joi.string().required().label('name'),
+  name: Joi.string().max(80).required().label('name'),
   locationId: Joi.string()
     .guid({
       version: ['uuidv4']
@@ -14,7 +14,7 @@ const createTreatmentProcessSchema = Joi.object({
     .regex(/^[0-9a-fA-F]{24}$/)
     .label('treatmentId'),
   note: Joi.string().max(150).allow(null, '').label('note'),
-  createOn: Joi.string().isoDate().label('createOn'),
+  createOn: Joi.date().label('createOn'),
   procedures: Joi.array()
     .items(
       Joi.object({
@@ -29,6 +29,7 @@ const createTreatmentProcessSchema = Joi.object({
       })
     )
     .min(1)
+    .required()
     .label('procedures'),
   prescription: Joi.object({
     diagnosis: Joi.string().required().label('diagnosis'),
@@ -65,8 +66,8 @@ const createTreatmentProcessSchema = Joi.object({
       .required()
       .label('staffId'),
     labo: Joi.string().required().label('labo'),
-    sentDate: Joi.string().isoDate().allow(null).label('sentDate'),
-    receivedDate: Joi.string().isoDate().allow(null).label('sentDate'),
+    sentDate: Joi.date().allow(null).label('sentDate'),
+    receivedDate: Joi.date().allow(null).label('sentDate'),
     diagnostic: Joi.string().required().label('diagnostic'),
     note: Joi.string().allow(null).label('note')
   })
@@ -78,39 +79,35 @@ const updateTreatmentProcessSchema = Joi.object({
     .required()
     .regex(/^[0-9a-fA-F]{24}$/)
     .label('treatmentProcessId'),
-  name: Joi.string().label('name'),
+  name: Joi.string().max(80).required().label('name'),
   locationId: Joi.string()
     .guid({
       version: ['uuidv4']
     })
-    .label('locationId'),
-  treatmentId: Joi.string()
     .required()
-    .regex(/^[0-9a-fA-F]{24}$/)
-    .label('treatmentId'),
+    .label('locationId'),
   note: Joi.string().max(150).allow(null, '').label('note'),
   createOn: Joi.date().label('createOn'),
-  createdById: Joi.string()
-    .guid({ version: ['uuidv4'] })
-    .required()
-    .label('createdById'),
   procedures: Joi.array()
     .items(
       Joi.object({
-        id: Joi.string().label('id'),
-        status: Joi.valid(...Object.values(EStatusProcedure)).label('status'),
-        detailTreatment: Joi.string().label('detailTreatment')
+        id: Joi.string().required().label('id'),
+        status: Joi.valid(...Object.values(EStatusProcedure))
+          .required()
+          .label('status'),
+        detailTreatment: Joi.string().required().label('detailTreatment')
       })
     )
     .min(1)
+    .required()
     .label('procedures'),
   prescription: Joi.object({
     prescriptionId: Joi.string()
       .regex(/^[0-9a-fA-F]{24}$/)
       .allow(null, '')
       .label('prescription'),
-    diagnosis: Joi.string().label('diagnosis'),
-    note: Joi.string().label('note'),
+    diagnosis: Joi.string().required().label('diagnosis'),
+    note: Joi.string().allow(null, '').label('note'),
     drugList: Joi.array()
       .min(1)
       .required()
@@ -119,11 +116,39 @@ const updateTreatmentProcessSchema = Joi.object({
           medicineId: Joi.string()
             .regex(/^[0-9a-fA-F]{24}$/)
             .label('medicineId'),
-          quantity: Joi.number().label('quantity'),
-          note: Joi.string().label('note')
+          quantity: Joi.number().integer().min(1).required().label('quantity'),
+          note: Joi.string().allow(null, '').label('note')
         })
       )
       .label('drugList')
-  }).label('prescription')
+  })
+    .allow(null)
+    .label('prescription'),
+  labo: Joi.object({
+    laboId: Joi.string()
+      .regex(/^[0-9a-fA-F]{24}$/)
+      .allow(null)
+      .label('laboId'),
+    status: Joi.string().required().valid('ordered', 'deliveried').label('status'),
+    customerId: Joi.string()
+      .guid({
+        version: ['uuidv4']
+      })
+      .required()
+      .label('customerId'),
+    staffId: Joi.string()
+      .guid({
+        version: ['uuidv4']
+      })
+      .required()
+      .label('staffId'),
+    labo: Joi.string().required().label('labo'),
+    sentDate: Joi.date().allow(null).label('sentDate'),
+    receivedDate: Joi.date().allow(null).label('sentDate'),
+    diagnostic: Joi.string().required().label('diagnostic'),
+    note: Joi.string().allow(null).label('note')
+  })
+    .allow(null)
+    .label('labo')
 });
 export { createTreatmentProcessSchema, updateTreatmentProcessSchema };
